37d8b80ae5a9f751ede828997bc8035b55f8f77e
Andrey Dotsenko <pgandrey@yandex.ru>
2011-08-31 16:35:13 +0400

    Reorganized

--- libnss-jabbernet/daemon.h
+++
@@ -1,8 +0,0 @@
-#include <sys/socket.h>
-
-int jabbernet_connectTo(sa_family_t family);
-
-
-enum nss_status jabbernet_getUser(int sock, char* name, const uid_t uid, struct passwd *pw, int *errnop,char *buffer, size_t buflen);
-enum nss_status jabbernet_getGroup(int sock,const char *name,const gid_t gid,struct group * gr, int * errnop,char * buffer, size_t buflen);
-enum nss_status jabbernet_getShadow (int sock, const char *name, struct spwd *spw, int * errnop, char *buffer, size_t buflen);
